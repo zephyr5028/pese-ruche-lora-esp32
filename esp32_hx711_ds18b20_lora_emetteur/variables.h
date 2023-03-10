4f33690f41eafcbb84e1bda6182771da1d07d235
zephyr5028 <marguerie.jl@wanadoo.fr>
2023-03-10 15:22:25 +0100

    ruche 03 correction alimentation

--- esp32_hx711_ds18b20_lora_emetteur/variables.h
+++ esp32_hx711_ds18b20_lora_emetteur/variables.h
@@ -15,14 +15,14 @@
 //=============
 //#define RUCHE_NUMERO  01      // numero de la ruche jlm1
 //#define RUCHE_NUMERO  02      // numero de la ruche jlm2
-//#define RUCHE_NUMERO  03      // numero de la ruche jlm3
+#define RUCHE_NUMERO  03      // numero de la ruche jlm3
 //#define RUCHE_NUMERO  11      // numero de la ruche loic1
 //#define RUCHE_NUMERO  12      // numero de la ruche loic2
 //#define RUCHE_NUMERO  13      // numero de la ruche loic3
 //#define RUCHE_NUMERO  14      // numero de la ruche loic4
 //#define RUCHE_NUMERO  15      // numero de la ruche loic5
 //#define RUCHE_NUMERO  16      // numero de la ruche loic6
-#define RUCHE_NUMERO  17      // numero de la ruche loic7
+//#define RUCHE_NUMERO  17      // numero de la ruche loic7
 //#define RUCHE_NUMERO  18      // numero de la ruche loic8
 //#define RUCHE_NUMERO  19      // numero de la ruche loic9
 
@@ -207,7 +207,7 @@ float correction = 0.15 ; // correction erreurs des resistances
 float correction = -0.35 ; // correction erreurs des resistances
 
 #elif RUCHE_NUMERO == 03
-float correction = -0.35 ; // correction erreurs des resistances
+float correction = -0.65 ; // correction erreurs des resistances
 
 #elif RUCHE_NUMERO == 11
 float correction = -0.35 ; // correction erreurs des resistances
@@ -225,7 +225,7 @@ float correction = -0.35 ; // correction erreurs des resistances
 float correction = -0.35 ; // correction erreurs des resistances
 
 #elif RUCHE_NUMERO == 16
-float correction = -0.25 ; // correction erreurs des resistances
+float correction = -0.35 ; // correction erreurs des resistances
 
 #elif RUCHE_NUMERO == 17
 float correction = -0.45 ; // correction erreurs des resistances
